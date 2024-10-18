77692e0dfdc1a4c3c5d9aba8d1d1e28fda0b059d
zlatakjshkarova <147971125+zlatakjshkarova@users.noreply.github.com>
2024-10-19 00:59:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+
+
+let sd = 20;
+let sdf
+if (20 >= 10) {
+    sdf = 20
+} else {
+    sdf = 0;
+}
+console.log(sdf = sd >= 20 ? 20 : 0);
+
+let df = 12;
+let dfg;
+if (df !== 20) {
+    dfg = 1;
+} else {
+    dfg = 0;
+}
+console.log(dfg = df !== 12 ? 1 : 0);
+
+
+
+
+
+
+
+
+let n = 8;
+n = parseFloat(n);
+let d = 8;
+d = parseFloat(d);
+nb = n + d;
+let wer
+wer = nb;
+console.log(`${nb * wer}`);
+
+
+
+
+
+
+
+let res;
+
+let num = 6;
+res = num >= 6 ? 0 : 5;
+console.log(`${num * res}`);
+
+let rer
+
+
+rer = num == 30 ? 10 : 20;
+console.log(`${num * rer}`);
+
+let num2 = 8;
+let e;
+e = num2 >= 8 ? 8 : 3;
+console.log(`${mun2 * e}`);
+
+
+
+let red;
+let num1 = 7;
+red = num1 == 7 ? 7 : 1;
+console.log(`${num1 * red}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
